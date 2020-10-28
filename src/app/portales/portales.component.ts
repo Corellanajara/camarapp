a06ac549b7dc107f660dd7df7b781fb73b11bd6d
Elías Navarrete <e.navagoro@gmail.com>
2020-10-28 01:22:19 -0300

    <feat>:<new portal selection>

--- src/app/portales/portales.component.ts
+++ src/app/portales/portales.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portales.component.css']
 })
 export class PortalesComponent implements OnInit {
-  items = ['Portal Inmobiliario']
+  items = [{title:'Portal Inmobiliario',url:'portalinmobiliario.com'},{title:'Yapo',url:'https://www.portalinmobiliario.com/'},{title:'Yapo',url:'https://www.portalinmobiliario.com/'},{title:'Portal Inmobiliario',url:'portalinmobiliario.com'},{title:'Yapo',url:'https://www.portalinmobiliario.com/'},{title:'Yapo',url:'https://www.portalinmobiliario.com/'}]
+  portal = '';
+  selectionFlag = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectItem(item){
+    this.portal = item;
+  }
 }
